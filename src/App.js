151f70ff0172d6d5a87f241a6b762c7bd68cbc5d
ellenchialin <chialin76@gmail.com>
2022-10-12 10:43:11 +0800

    feat: add maintenance homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import s from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.app}>
+      <Header />
+      <div className={s.content}>
+        <div className={s.message}>
+          <h2 className={s.title}>We'll be back soon! </h2>
+          <p>
+            The website is under maintenance. Please join{' '}
+            <a
+              href='https://discord.com/invite/lootexio'
+              className={s.link}
+              target='_blanck'
+            >
+              Our Discord
+            </a>{' '}
+            or check out the latest information on{' '}
+            <a
+              href='https://twitter.com/LootexIO'
+              className={s.link}
+              target='_blanck'
+            >
+              Twitter
+            </a>
+            .
+          </p>
+        </div>
+        <div className={s.imageWrapper}>
+          <img src='./assets/pic_maintenance.png' alt='maintenance' />
+        </div>
+      </div>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
